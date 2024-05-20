bb9d063305f35d0db434a14cfa3628dc664780a3
martins86 <martins86@gmail.com>
2024-05-20 20:57:49 +0000

    layout details

--- modules/users/src/lib/shared/components/user-details/user-details.component.ts
+++ modules/users/src/lib/shared/components/user-details/user-details.component.ts
@@ -1,10 +1,13 @@
 import { Component } from '@angular/core';
 
+import { MatCardModule } from '@angular/material/card';
+import { MatButtonModule } from '@angular/material/button';
+
 @Component({
   selector: 'lib-user-details',
-  standalone: true,
-  imports: [],
   templateUrl: './user-details.component.html',
   styleUrl: './user-details.component.scss',
+  standalone: true,
+  imports: [MatCardModule, MatButtonModule],
 })
 export class UserDetailsComponent {}
